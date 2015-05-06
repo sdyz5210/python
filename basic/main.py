017837369b8e99640ad88d90877cef6c3a6b359a
sdyz5210 <sdyz5210@gmail.com>
2015-05-06 20:33:14 +0800

    修改文件夹gui为tkinter

---
+++ basic/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/evn python
+# -*- coding: utf-8 -*-
+
+def test():
+	print '测试main函数'
+
+if __name__ =='__main__':
+	test()
--- easyui/easyguitest.py
+++ easyui/easyguitest.py
@@ -1,3 +1,4 @@
+#!/usr/bin/evn python
 # -*- coding: utf-8 -*-  
 
 import easygui as g
