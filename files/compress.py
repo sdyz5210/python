45f3d757a5eef5e546b09a88f1666c5d4cda8a99
sdyz5210 <sdyz5210@gmail.com>
2015-09-15 11:12:24 +0800

    修改压缩算法

--- files/compress.py
+++ files/compress.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 # python version 2.7.6
 
-import gzip,shutil
+import gzip,shutil,os
 
-def zipCompress():
-	pass
-
-def gzipCompress():
-	with gzip.open('1.gz', 'wb') as f_out,open('/Users/mac/Documents/data/1.fastq','rb') as f_in:
+def gzipCompress(fileName):
+	#生成压缩后的文件名
+	tarFileName = fileName+".gz"
+	with gzip.open(tarFileName, 'wb') as f_out,open(fileName,'rb') as f_in:
 		shutil.copyfileobj(f_in, f_out)
 
-gzipCompress()
+def gzipUnCompress(tarFileName):
+	fileName = os.path.splitext(tarFileName)[0]
+	print fileName
+	with gzip.open(tarFileName, 'rb') as f_in,open(fileName,'wb') as f_out:
+		shutil.copyfileobj(f_in, f_out)
+
+#if __name__ == '__main__':
+	#gzipCompress("/Users/mac/Documents/data/1.fastq")
+	#gzipUnCompress("/Users/mac/Documents/data/4.fastq.gz")
